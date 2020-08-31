fe15ba1035c8e1d9b1b7b5932ce9efa6e4a11e80
cassfer <user.email>
2020-08-31 18:12:56 -0300

    formulario iniciado

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
-class Settings extends StatelessWidget{
+class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Configurações'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Configurações'),
+        ),
+        body: Container(
+          margin: const EdgeInsets.all(15),
+          child: Form(
+            child: Column(
+              children: [TextFormField()],
+            ),
+          ),
+        ));
   }
-  
 }
